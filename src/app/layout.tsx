cf3a8a0c6b0e2c64783f5d5dae11e74c9bd6e737
Dario Dumlijan <dario.dumlijan1@gmail.com>
2024-08-01 11:32:53 +0200

    fix: index and removed location in bio

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import ScrollToTop from '@/components/ScrollToTop';
 import '../styles/index.scss';
 
 export const metadata: Metadata = {
+  robots: 'index, follow',
   title: 'Chimera Studio™',
   description: 'We are an app developing collective armed with a wide spread of digital skills, ready to deliver fun and entertainment. Our recent leaps in progress have allowed us to offer in-organisation project optimisations and idea realisations. It\'s simple, if you have a good idea for an app, talk to us.',
   keywords: 'HTML, CSS, JavaScript, App, Apps, React, React Native, Application development, software development, web development, design, UI/UX design, graphic design, Dario Dumlijan, Boris Vida, Damjan Gazibaric, Chimera Studio',
